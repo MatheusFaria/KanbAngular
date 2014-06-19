13e36800503f7d3ac1d3b296e6f81b92c0e2a2b4
MatheusFaria <matheus.sousa.faria@gmail.com>
2014-06-19 14:49:52 -0300

    Colocando comportamento de repeticao para a story e criando a controller de story

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 (function() {
   var app = angular.module('kanbAngular', []);
 
+  app.controller("StoryController", function() {
+    this.stories = stories;
+
+    this.addStory = function() {
+
+    };
+  });
+
   app.directive("story", function(){
     return {
             restrict: 'E',
             templateUrl: "story.html"
+    };
+  });
+
+  app.directive("storyForm", function(){
+    return {
+            restrict: 'E',
+            templateUrl: "story-form.html"
     };
   });
 
@@ -35,13 +50,6 @@
     };
   });
 
-  app.directive("storyForm", function(){
-    return {
-            restrict: 'E',
-            templateUrl: "story-form.html"
-    };
-  });
-
   app.directive("taskForm", function(){
     return {
             restrict: 'E',
@@ -56,8 +64,19 @@
     };
   });
 
-  /*app.controller('StoryController', function(){
-    this.title = "ddddddd";
-  });*/
+  var stories = [{
+      id: 1,
+      as: "a user",
+      iWant: "to do my homework",
+      to: "pass in DAS",
+      tasks: []
+    },
+    {
+      id: 2,
+      as: "a user",
+      iWant: "to do my homework",
+      to: "pass in DAS",
+      tasks: []
+    }];
 
 })();
