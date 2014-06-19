a02adaac644a19d7f71324e74196797386f69bbd
Ana Paula <anapaulavnoronha@gmail.com>
2014-06-19 13:31:47 -0300

    Populando as abas com suas diretivas

--- app.js
+++ app.js
@@ -15,6 +15,47 @@
     };
   });
 
+  app.directive("formTabs", function(){
+    return {
+            restrict: 'E',
+            templateUrl: "form-tabs.html",
+              
+            controller: function() {
+              this.tab = 3;
+              this.isSet = function(checkTab) {
+                return this.tab === checkTab;
+              };
+
+              this.setTab = function(activeTab) {
+                this.tab = activeTab;
+              };  
+              
+            },
+            controllerAs: "tab"
+    };
+  });
+
+  app.directive("storyForm", function(){
+    return {
+            restrict: 'E',
+            templateUrl: "story-form.html"
+    };
+  });
+
+  app.directive("taskForm", function(){
+    return {
+            restrict: 'E',
+            templateUrl: "task-form.html"
+    };
+  });
+
+  app.directive("history", function(){
+    return {
+            restrict: 'E',
+            templateUrl: "history.html"
+    };
+  });
+
   /*app.controller('StoryController', function(){
     this.title = "ddddddd";
   });*/
