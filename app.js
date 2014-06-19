2aff226d111a461776c3f002fa00873d5c77b647
Leandro Veloso <leandrovelosorodrigues@gmail.com>
2014-06-19 13:05:34 -0300

    Criando estrutura de abas e aplicando bootstrap

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 (function() {
   var app = angular.module('kanbAngular', []);
+
   app.directive("story", function(){
     return {
             restrict: 'E',
             templateUrl: "story.html"
     };
   });
+
+  app.directive("task", function(){
+    return {
+            restrict: 'E',
+            templateUrl: "task.html"
+    };
+  });
+
+  /*app.controller('StoryController', function(){
+    this.title = "ddddddd";
+  });*/
 
 })();
